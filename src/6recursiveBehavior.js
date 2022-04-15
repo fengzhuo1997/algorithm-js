6a36f18b9508aa714b04dddb6f90a8a3e235ee73
Zhuo Feng <fengzo@cn.ibm.com>
2022-04-15 09:53:32 +0800

    update

--- src/6recursiveBehavior.js
+++ src/6recursiveBehavior.js
@@ -1,12 +1,25 @@
 /*
  * 6.递归行为
- * 
+ * O(log2为底N的对数)
  */
 
-var arr = [3, 2, 5, 6, 7, 4];
+var arr = [3, 3, 5, 6, 7, 4, 100, 876, 654, 7896, 8, 3]; // 找出数组中最大的值
 
+var recursive = (arr) => {
+  var newArr = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    if (arr[i + 1]) {
+      arr[i] >= arr[i + 1] ? newArr.unshift(arr[i]) : newArr.unshift(arr[i + 1]);
+    } else {
+      newArr.unshift(arr[i]);
+    }
+  }
+  if (newArr.length == 1) {
+    console.log("最大数字是:" + newArr[0]);
+    return newArr[0];
+  } else {
+    recursive(newArr);
+  }
+};
 
-var recursive = () => {
-
-    
-}
+recursive(arr);
